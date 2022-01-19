d6b404bdabd2c339f47bed2ffd2427c09665ff9d
Alisha <irl_a@mail.ru>
2022-01-19 15:23:41 +0300

    -histogram

--- Erlang/Erlang/Erlang/Program.cs
+++ Erlang/Erlang/Erlang/Program.cs
@@ -11,17 +11,17 @@ namespace Erlang
 
             while (mu <= 0)
             {
-                Console.WriteLine("Please, enter value of mu= ");
+                Console.Write("Please, enter value of mu= ");
                 mu = float.Parse(Console.ReadLine()); //2
             }
 
             while (la <= 0)
             {
-                Console.WriteLine("Please, enter value of la= ");
+                Console.Write("Please, enter value of la= ");
                 la = float.Parse(Console.ReadLine()); //1
             }
 
-            Console.WriteLine("Please, enter number of iteration N= ");
+            Console.Write("Please, enter number of iteration N= ");
             int N = int.Parse(Console.ReadLine());
 
             List<Point> points = new();
@@ -41,20 +41,27 @@ namespace Erlang
                 point.Y = Convert.ToSingle(mu * la * (Math.Exp(-mu * point.X) - Math.Exp(-la * point.Y)) / (la - mu));
             }
 
-            List<Point> Upoints = new(N+1);
+            List<Point> Upoints = new();
+
+            for (int i = 0; i <= N + 1; i++)
+            {
+                Upoints.Add(new Point());
+            }
             
             for (int i = 1; i <= N; i++)
             {
-                Upoints[i] = points[i - 1];
+                Upoints[i].X = points[i - 1].X;
+                Upoints[i].Y = points[i - 1].Y;
             }
-            Upoints[0].X = 0;
-            Upoints[0].Y = 0;
-            Upoints[N+1].X = 1;
-            Upoints[N+1].Y = 1;
+            Upoints[0].X = 0.0f;
+            Upoints[0].Y = 0.0f;
+            Upoints[N+1].X = 1.0f;
+            Upoints[N+1].Y = 1.0f;
             Upoints.Sort();
 
             float Sherman = 0;
-            float alpha = 0.05f;
+            Console.Write("Please, enter value of alpha= "); //0.95f
+            float alpha = float.Parse(Console.ReadLine());
 
             for (int i = 1; i <= N ; i++)
             {
@@ -62,8 +69,20 @@ namespace Erlang
             }
             Sherman = Sherman / 2;
 
+            Console.Write($"Please, enter the critical value of Sherman statistics at a={alpha} and N={N}: ");
+            float ShermanC = float.Parse(Console.ReadLine());
+
+            if (Sherman < ShermanC)
+            {
+                Console.WriteLine("The sample corresponds to the distribution.");
+            }
+            else
+            {
+                Console.WriteLine("The sample doesn't correspond to the distribution.");
+            }
+
             //float ShermanN = Convert.ToSingle((Sherman - Math.Pow(N / (N + 1), N + 1)) / ((Math.Pow(2 * N, N + 2) + N * Math.Pow(N - 1, N + 2)) / (N + 2) * Math.Pow(N + 1, N + 2) - Math.Pow(N / (N + 1), 2 * N + 2)));
-            
+
         }
     }
 }
